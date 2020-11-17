e59bb952350b9fb86b4f3eedace4566256a14dfb
Jose Carlos Viales Guevara <josec15vg@gmail.com>
2020-11-16 20:05:28 -0600

    Ejemplo de JqueyCarrusel

--- CodigoJS.js
+++ CodigoJS.js
@@ -4,3 +4,28 @@ if ('serviceWorker' in navigator) {
       .then(reg => console.log('Registro de SW exitoso', reg))
       .catch(err => console.warn('Error al tratar de registrar el sw', err))
   }
+
+  $(document).ready(function() {
+    var carousel = $("#carousel").featureCarousel({
+      trackerSummation: false
+      // include options like this:
+      // (use quotes only for string values, and no trailing comma after last option)
+      // option: value,
+      // option: value
+    });
+
+    $("#but_prev").click(function () {
+      carousel.prev();
+    });
+    $("#but_pause").click(function () {
+      carousel.pause();
+    });
+    $("#but_start").click(function () {
+      carousel.start();
+    });
+    $("#but_next").click(function () {
+      carousel.next();
+    });
+
+  });
+  
